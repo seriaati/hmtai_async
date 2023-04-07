43fd3755e898d8ccf07989f3095ba5d1c0ed28ed
seriaati <seria.ati@gmail.com>
2023-04-07 16:12:48 +0800

    feat: add docs and combine functions

--- client.py
+++ client.py
@@ -1,33 +1,41 @@
-from typing import List, Optional
+import typing
+
 import aiohttp
-import json
+
+from .enum import EndpointType
 
 
 class HmtaiAPI:
     def __init__(self, session: aiohttp.ClientSession):
         self.session = session
 
-    async def update_endpoints(self) -> None:
+    async def get_endpoints(self, type: EndpointType) -> typing.List[str]:
+        """Get all endpoints of a certain type.
+
+        Args:
+            type (EndpointType): The type of endpoints to get.
+
+        Returns:
+            typing.List[str]: A list of endpoints.
+        """
         async with self.session.get("https://hmtai.hatsunia.cfd/v2/endpoints") as resp:
-            endpoints = await resp.json()
-        with open("endpoints.json", "w") as f:
-            json.dump(endpoints, f, indent=4)
+            endpoints: typing.Dict[str, typing.Any] = await resp.json()
+        return endpoints.get(type.value, [])
 
-    async def get_sfw_endpoints(self) -> List[str]:
-        with open("endpoints.json", "r") as f:
-            endpoints = json.load(f)
-        return endpoints["nsfw"]
+    async def get_image(self, endpoint: str) -> typing.Optional[str]:
+        """Get a random image from a certain endpoint.
 
-    async def get_nsfw_endpoints(self) -> List[str]:
-        with open("endpoints.json", "r") as f:
-            endpoints = json.load(f)
-        return endpoints["nsfw"]
+        Args:
+            endpoint (str): The endpoint to get the image from.
 
-    async def get(self, endpoint: str) -> Optional[str]:
+        Returns:
+            typing.Optional[str]: The URL of the image. None if the endpoint is invalid.
+        """
         async with self.session.get(
             f"https://hmtai.hatsunia.cfd/v2/{endpoint}"
         ) as resp:
             data = await resp.json()
         if "url" not in data:
             return None
+
         return data["url"]
